e00473865d489c88fb39ba5cc44fecc247980e03
damian <damibdnk62@gmail.com>
2025-12-02 15:16:31 +0700

    Add files via upload

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'nama' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('123123123'),
+            'jabatan' => 'Admin',
+            'is_tugas' => false,
+        ];
+    }
+}
