8a13ba4fd54fd797ff0f711a6cd9178dc25cab1d
damian <damibdnk62@gmail.com>
2025-12-02 15:20:49 +0700

    Add files via upload

---
+++ storage/framework/views/43056862f91297537e547b2460898a97.php
@@ -0,0 +1,31 @@
+
+
+
+<?php $__env->startSection('content'); ?>
+    <h1 class="h3 mb-4 text-gray-800">
+        <i class="fas fa-tachometer-alt mr-2"></i>
+        <?php echo e($title); ?>
+
+    </h1>
+
+    <div class="row">
+        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                TOTAL USER</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">1</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-user fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+ 
+<?php echo $__env->make('layout/app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\laragon\www\manajemen_tugas\resources\views/dashboard.blade.php ENDPATH**/ ?>
